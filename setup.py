14b55f76eaf2699952a9bb7c5d699c382b53f874
Thomas Roder <thomas.roder@bioinformatics.unibe.ch>
2020-03-04 14:32:47 +0100

    remove long description for pypi

--- setup.py
+++ setup.py
@@ -4,17 +4,13 @@ from os import path
 # Instructions:
 # https://medium.com/@joel.barmettler/how-to-upload-your-python-package-to-pypi-65edc5fe9c56
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
 setup(
     name='binary_file_search',
     packages=['binary_file_search'],
     version='0.1',
     license='MIT',
     description='Binary search algorithm for big sorted files that cannot be read into RAM.',
-    long_description_content_type='text/markdown',
-    long_description=long_description,
+    long_description='Please read the descriptionon on the github page.',
     author='Thomas Roder',
     author_email='roder.thomas@gmail.com',
     url='https://github.com/MrTomRod/BinaryFileSearch',
